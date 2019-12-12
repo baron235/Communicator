8dbac11d57836a9c48d4283e3562732d55c239e8
Rafał Stempowski <stempowski132@gmail.com>
2019-12-12 11:50:51 +0100

    Update MessageService.cs

--- Communicator-Backend/Communicator-Backend/Services/MessageService.cs
+++ Communicator-Backend/Communicator-Backend/Services/MessageService.cs
@@ -10,21 +10,26 @@ namespace Communicator_Backend.Services
     public class MessageService : IMessageService
     {
         private readonly IMessageRepository messageRepository;
+        private readonly IUserRepository userRepository;
 
-        public MessageService(IMessageRepository messageRepository)
+        public MessageService(IMessageRepository messageRepository, IUserRepository userRepository)
         {
             this.messageRepository = messageRepository;
+            this.userRepository = userRepository;
         }
 
         public void AddTextMessage(string fromLogin, string toLogin, string text)
         {
+            var idFrom = this.userRepository.GetUser(fromLogin).Id;
+            var idTo = this.userRepository.GetUser(toLogin).Id;
+            
             var m = new Message();
             m.Content = text;
-
+            m.isRead = false;
+            m.FromUserId idFrom;
+            // TODO dokoncz jeszcze ToUserId, SendTime i message type :p
+            
             this.messageRepository.AddMessage(m);
-
         }
-
-       
     }
 }
